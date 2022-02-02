1e8f9bb8aa1cc14c67db0ea9b7b2207f802b90be
Jaylin Gross <grossjaylin@gmail.com>
2022-02-02 15:17:42 -0500

    removed burger bar in header

--- src/components/Data.js
+++ src/components/Data.js
@@ -1,22 +1,3 @@
 export const Data = [
-    {
-        text : 'Home',
-        path : '/',
-        className : 'data-item'
-    },
-    {
-        text : 'Works',
-        path : '/works',
-        className : 'data-item'
-    },
-    {
-        text : 'Gallery',
-        path : '/gallery',
-        className : 'data-item'
-    },
-    {
-        text : 'Repositories',
-        path : '/repos',
-        className : 'data-item'
-    },
+   
 ]
